a0c8ef5eb8624487ce55856ef5d6eb7ad1121150
Elena Torro <elenatorro@gmail.com>
2014-08-11 10:31:58 +0200

    projets and tasks created

---
+++ app/models/task.rb
@@ -0,0 +1,7 @@
+class Task < ActiveRecord::Base
+	belongs_to :project
+
+	validates :name, presence: true
+	validates :status, inclusion: { in: ["todo", "doing", "done"]),
+    message: "%{value} is not a valid status" }
+end
